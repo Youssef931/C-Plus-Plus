fd64259535f3f5064663a03ce80f44d5b74bf89d
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-13 00:19:57 +0200

    New Delete
    <<Ade << Nasim << OPP - 7;

---
+++ New Delete/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+//class ARectangle {
+//	int* width, * height;
+//public:
+//	ARectangle(int, int);
+//	~ARectangle();
+//	int area()
+//	{
+//		return (*width * *height);
+//	}
+//};
+//ARectangle::ARectangle(int a, int b)
+//{
+//	width = new int;
+//	height = new int;
+//	*width = a;
+//	*height = b;
+//}
+//ARectangle::~ARectangle()
+//{
+//	delete width;
+//	delete height;
+//}
+int main()
+{
+	/*int* p1, * p2;
+	p1 = new int;
+	*p1 = 10;
+	cout << &p1 << endl;
+	cout << "memory location : " << p1 << endl;
+	cout << "contains : " << *p1 << endl;
+	p2 = new int;
+	*p2 = 10;
+	cout << &p2 << endl;
+	cout << "memory location : " << p2 << "\n" << "contains : " << *p2 << endl;
+	delete p1, p2;*/
+	/*ARectangle rect(3, 4), recta(5, 6);
+	cout << "rect area : " << rect.area() << endl;
+	cout << "recta area : " << recta.area() << endl;*/
+}
