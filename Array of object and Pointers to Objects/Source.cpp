8ee5270e3dfc6fa9e826843a6fe949a4cfd644da
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-15 00:29:30 +0200

    Array of object and Pointers to Objects
    Adel<<Nasim<<OPP<<-8;

---
+++ Array of object and Pointers to Objects/Source.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+//class student {
+//	char name[20];
+//	int ID;
+//public:
+//	student()
+//	{
+//		cout << "empty\n";
+//		strcpy_s(name, "No Name");
+//		ID = 0;
+//	}
+//	student(char n[], int id)
+//	{
+//		cout<<"Paramatrize\n";
+//		strcpy_s(name, n);
+//		ID = id;
+//	}
+//	void print(void)
+//	{
+//		cout << name << "\t" << ID << endl;
+//	}
+//};
+int main()
+{
+	//int arr[5] = { 10,20,30,40,50 };
+	//int *p;
+	//p = arr;
+	//cout << *(p + 1) << endl;
+	//cout << *(p + 3) << endl;
+	//cout << arr << endl;
+	//cout << *(arr+4) << endl;
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	cout << *(arr+i) << endl;
+	//}
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	cout << *(p + i) << endl;
+	//}
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	cout << *(p++) << endl;
+	//}
+	//student S1((char*)"Aly", 111111), S2((char*)"Ahmed", 222222),*p;
+	//student arr[3] = { student((char*)"Adel",333333) };
+	//student arr[3] = { S1,S2 };
+	//p = arr;
+	//int i = 0;
+	//(p + i)->print();
+	//i++;
+	//(p + i)->print();
+	//i++;
+	//(p + i)->print();
+	//i++;
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	p ++-> print();
+	//}
+}
