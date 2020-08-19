ff9fb3c656d435517a43cdf505be0ccf102d2f7d
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-20 00:57:14 +0200

    Review Structure
    Adel<<Nasim<<OPP<<6;

---
+++ Review Structure/Review Structure/Source.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+using namespace std;
+//1 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//};
+//*********************************************************
+//2 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//};
+//*********************************************************
+//3 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//};
+//*********************************************************
+//4 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//}g,k;
+//*********************************************************
+//5 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//};
+//*********************************************************
+//6 - struct car {
+//	string name;
+//	string color;
+//	int maxspeed;
+//	int model;
+//};
+//*********************************************************
+//7 - struct Distance
+//{
+//	int feet;
+//	float inchaes;
+//};
+//Distance add_distance(Distance d1, Distance d2)
+//{
+//	Distance result;
+//	result.feet = d1.feet + d2.feet;
+//	result.inchaes = d1.inchaes + d2.inchaes;
+//	return result;
+//}
+//*********************************************************
+//8 - struct exam
+//{
+//	float first;
+//	float second;
+//	float final;
+//};
+//class Subject
+//{
+//	char name[20];
+//	exam Exam;
+//public:
+//	Subject()
+//	{
+//		strcpy_s(name, "No name");
+//		Exam = { 0,0,0 };
+//	}
+//	Subject(char n[], float f1, float f2, float f3)
+//	{
+//		strcpy_s(name, n);
+//		Exam = { f1,f2,f3 };
+//	}
+//	float total()
+//	{
+//		return Exam.first + Exam.second + Exam.final;
+//	}
+//	void print()
+//	{
+//		cout << "The Subject is " << name << "\n"
+//			<< "First Exam = " << Exam.first << "\n"
+//			<< "Second Exam = " << Exam.second << "\n"
+//			<< "Final Exam = " << Exam.final << endl;
+//	}
+//	};
+//*********************************************************
+//9 - struct exam
+//{
+//	float first;
+//	float second;
+//	float final;
+//	void f() { cout << "Struct"; }
+//};
+//class Subject
+//	{
+//		char name[20];
+//		exam Exam;
+//	public:
+//		Subject()
+//		{
+//			strcpy_s(name, "No name");
+//			Exam = { 0,0,0 };
+//		}
+//		Subject(char n[], float f1, float f2, float f3)
+//		{
+//			strcpy_s(name, n);
+//			Exam = { f1,f2,f3 };
+//		}
+//		float total()
+//		{
+//			return Exam.first + Exam.second + Exam.final;
+//		}
+//		void print()
+//		{
+//			cout << "The Subject is " << name << "\n"
+//				<< "First Exam = " << Exam.first << "\n"
+//				<< "Second Exam = " << Exam.second << "\n"
+//				<< "Final Exam = " << Exam.final << endl;
+//		}
+//		Subject(char n[], float f1, float f2, float f3)
+//		{
+//			Exam.f();
+//			strcpy_s(name, n);
+//			Exam = { f1,f2,f3 };
+//		}
+//		};
+int main()
+{
+	/*1 - car g;
+	g.name = "Kia";
+	g.color = "Red";
+	cout << g.name << endl;*/
+	//*****************************************************
+	/*2 - car g;
+	g.name = "Kia";
+	g.color = "Red";
+	cout << g.color << endl;*/
+	//*****************************************************
+	/*3 - int arr[5] = { 10,20,30,40,50 };
+	cout << arr[0] << endl;*/
+	//*****************************************************
+	/*4 - k = { "aa", "bl", 300, 97 };
+	g = { "Kia","Red",250, 96 };
+	cout << g.maxspeed << endl;*/
+	//*****************************************************
+	/*5 - car g = { "Kia","Red",250,96 },k;
+	if (g < k)
+		cout << "G is less than K";*/
+	//*****************************************************
+	/*6 - car g = { "Kia","Red",250,96 },k;
+	if (g.model < k.model)
+		cout << "G is less than K";*/
+	//*****************************************************
+	/*7 - Distance x, y, z;
+	cout << "Enter feet value\n";
+	cin >> x.feet >> y.feet;
+	cout << "Enter inches value \n";
+	cin >> x.inchaes >> y.inchaes;
+	z = add_distance(x, y);
+	cout << "z.feet = " << z.feet << ", z.inches " << z.inchaes << endl;*/
+	//*****************************************************
+	/*8 - Subject e((char*)"OPP", 25, 24, 49);
+	e.print();*/
+	//*****************************************************
+}
